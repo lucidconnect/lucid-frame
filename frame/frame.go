584b28eca6ed6e50b960d23b40048785b996edff
olawolu <gbemigaolawolu@gmail.com>
2024-02-27 15:55:07 +0100

    comment out code that handles AA address creation

--- frame/frame.go
+++ frame/frame.go
@@ -2,16 +2,11 @@ package frame
 
 import (
 	"fmt"
-	"log"
-	"math/big"
-	"os"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/google/uuid"
-	"github.com/lucidconnect/silver-arrow/abi/KernelFactory"
-	"github.com/lucidconnect/silver-arrow/core/service/erc4337"
+
+	// "github.com/lucidconnect/silver-arrow/abi/KernelFactory"
+	// "github.com/lucidconnect/silver-arrow/core/service/erc4337"
 	"gorm.io/gorm"
 )
 
@@ -44,9 +39,9 @@ type ClaimFrame struct {
 func CreateClaimFrame(itemId, imageUrl, collectionAddr string, db *gorm.DB) (string, error) {
 	id := uuid.New()
 	frame := ClaimFrame{
-		ID:       id,
-		ItemId:   itemId,
-		ImageUrl: imageUrl,
+		ID:                id,
+		ItemId:            itemId,
+		ImageUrl:          imageUrl,
 		CollectionAddress: collectionAddr,
 	}
 
@@ -145,74 +140,74 @@ func ParseFrameAction(btn Button, item, verifiedAddress string) (string, error) 
 	return response, nil
 }
 
-func CalculateCounterFactualAddress(farcasterSigner, kernelFactoryAddress, rpc string) (string, error) {
-	ownerBytes, err := hexutil.Decode(farcasterSigner)
-	if err != nil {
-		log.Println(err)
-	}
+// func CalculateCounterFactualAddress(farcasterSigner, kernelFactoryAddress, rpc string) (string, error) {
+// 	ownerBytes, err := hexutil.Decode(farcasterSigner)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
 
-	initCode, err := getContractInitCode(ownerBytes, common.Big0, kernelFactoryAddress)
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println("account initcode", hexutil.Encode(initCode))
+// 	initCode, err := getContractInitCode(ownerBytes, common.Big0, kernelFactoryAddress)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	fmt.Println("account initcode", hexutil.Encode(initCode))
 
-	kernelFactory := common.HexToAddress(kernelFactoryAddress)
-	backend := getEthBackend(rpc)
+// 	kernelFactory := common.HexToAddress(kernelFactoryAddress)
+// 	backend := getEthBackend(rpc)
 
-	accountFactoryCaller, err := KernelFactory.NewKernelFactoryCaller(kernelFactory, backend)
-	if err != nil {
-		log.Println(err)
-	}
+// 	accountFactoryCaller, err := KernelFactory.NewKernelFactoryCaller(kernelFactory, backend)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
 
-	walletAddress, err := accountFactoryCaller.GetAccountAddress(nil, initCode, common.Big0)
-	if err != nil {
-		return "", err
-	}
-	fmt.Println("account address", walletAddress)
+// 	walletAddress, err := accountFactoryCaller.GetAccountAddress(nil, initCode, common.Big0)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	fmt.Println("account address", walletAddress)
 
-	return walletAddress.Hex(), nil
-}
+// 	return walletAddress.Hex(), nil
+// }
 
-func getEthBackend(rpc string) *ethclient.Client {
-	conn, err := ethclient.Dial(rpc)
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
-	}
-	return conn
-}
+// func getEthBackend(rpc string) *ethclient.Client {
+// 	conn, err := ethclient.Dial(rpc)
+// 	if err != nil {
+// 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+// 	}
+// 	return conn
+// }
 
-func getContractInitCode(owner []byte, index *big.Int, factoryAddress string) ([]byte, error) {
-	initCode := []byte{}
-	implementation := os.Getenv("KERNEL_IMPLEMENTATION_ADDRESS")
-	defaultValidator := os.Getenv("DEFAULT_VALIDATOR")
+// func getContractInitCode(owner []byte, index *big.Int, factoryAddress string) ([]byte, error) {
+// 	initCode := []byte{}
+// 	implementation := os.Getenv("KERNEL_IMPLEMENTATION_ADDRESS")
+// 	defaultValidator := os.Getenv("DEFAULT_VALIDATOR")
 
-	kernelImplementation := common.HexToAddress(implementation)
-	// fmt.Println("accountAddress ", accountAddress)
+// 	kernelImplementation := common.HexToAddress(implementation)
+// 	// fmt.Println("accountAddress ", accountAddress)
 
-	/** inputs to createAddress
-		- account implementation
-		- calldata:abi.encodeWithSelector(
-	 		KernelStorage.initialize.selector, defaultValidator, abi.encodePacked(owner)),
-		- index
-	*/
+// 	/** inputs to createAddress
+// 		- account implementation
+// 		- calldata:abi.encodeWithSelector(
+// 	 		KernelStorage.initialize.selector, defaultValidator, abi.encodePacked(owner)),
+// 		- index
+// 	*/
 
-	callData, err := erc4337.EncodeKernelStorageWithSelector("initialize", common.HexToAddress(defaultValidator), owner)
+// 	callData, err := erc4337.EncodeKernelStorageWithSelector("initialize", common.HexToAddress(defaultValidator), owner)
 
-	fmt.Println("callData", hexutil.Encode(callData))
-	if err != nil {
-		return nil, err
-	}
+// 	fmt.Println("callData", hexutil.Encode(callData))
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	data := owner
-	fmt.Println("enable data ", hexutil.Encode(data))
-	code, err := erc4337.GetCreateAccountFnData(kernelImplementation, callData, index)
-	if err != nil {
-		return nil, err
-	}
-	factoryAddressToBytes := common.FromHex(factoryAddress)
-	initCode = append(initCode, factoryAddressToBytes...)
-	initCode = append(initCode, code...)
+// 	data := owner
+// 	fmt.Println("enable data ", hexutil.Encode(data))
+// 	code, err := erc4337.GetCreateAccountFnData(kernelImplementation, callData, index)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	factoryAddressToBytes := common.FromHex(factoryAddress)
+// 	initCode = append(initCode, factoryAddressToBytes...)
+// 	initCode = append(initCode, code...)
 
-	return initCode, nil
-}
+// 	return initCode, nil
+// }
--- go.mod
+++ go.mod
@@ -1,3 +1,24 @@
 module lucid.frame
 
 go 1.20
+
+require (
+	github.com/ethereum/go-ethereum v1.13.14
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+	google.golang.org/protobuf v1.32.0
+	gorm.io/driver/postgres v1.5.6
+	gorm.io/gorm v1.25.7
+)
+
+require (
+	github.com/holiman/uint256 v1.2.4 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
